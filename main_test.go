a72dab0af4e3a6c72c0ae297612ecfa25e48ce05
谢小军 <xxj@xiexiaojundeMacBook-Pro.local>
2019-09-15 22:44:08 +0800

    add test

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/xxjwxc/gowp/workerpool"
+)
+
+func TestMain(t *testing.T) {
+	wp := workerpool.New(5)   //设置最大线程数
+	for i := 0; i < 10; i++ { //开启10个请求
+		ii := i
+		wp.Do(func() error {
+			for j := 0; j < 5; j++ { //每次打印0-10的值
+				fmt.Println(fmt.Sprintf("%v->\t%v", ii, j))
+				time.Sleep(1 * time.Second)
+			}
+			return nil
+		})
+
+		fmt.Println(wp.IsDone())
+	}
+
+	wp.Wait()
+	fmt.Println(wp.IsDone())
+
+	fmt.Println("down")
+}
